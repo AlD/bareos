e9a2a87a02834825544cfcbbbca51f084b75583f
Marco van Wieringen <marco.van.wieringen@bareos.com>
2013-04-30 18:23:51 +0200

    Imported bacula-5.2.13 as downloaded from sourceforge.
    Released on 19/2/2013.

--- gui/bweb/script/update_job_old.sql
+++
@@ -1,8 +0,0 @@
--- First, create JobHistory table
--- CREATE TABLE JobHistory (LIKE Job);
--- 
--- then put this on your crontab
--- */20 * * * * psql -f /opt/bacula/etc/update_job_old.sql > /home/bacula/update_job_old.log
-INSERT INTO JobHistory
-  (SELECT * FROM Job WHERE JobStatus in ('T', 'f', 'A') AND JobId NOT IN (SELECT JobId FROM JobHistory) );
-
--- gui/bweb/tpl/error.tpl
+++
@@ -1,4 +0,0 @@
-<h1>__An error has occurred:__</h1>
-<pre>
-<TMPL_VAR error>
-</pre>
